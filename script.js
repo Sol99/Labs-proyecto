6481cdd83e619c23e9c1c04438bc22c2b3e40240
sol <caceressol@hotmail.com.ar>
2021-07-15 17:37:37 -0300

    Agrega script

---
+++ script.js
@@ -0,0 +1,11 @@
+var app1 = new Vue({
+    el:"#app",
+    data: {
+        cards: [
+            { title: "titulo1", description: "Descripcion del titulo 1" },
+            { title: "titulo2", description: "Descripcion del titulo 2" },
+            { title: "titulo3", description: "Descripcion del titulo 3" },
+            { title: "titulo4", description: "Descripcion del titulo 4" },
+        ]
+    }
+})
